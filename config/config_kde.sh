8677307334224fe0c8e0b178edd0cb1bcda002bb
Dmitry Savosh <d.savosh@gmail.com>
2023-10-07 01:17:35 +0300

    kde theme

--- config/config_kde.sh
+++ config/config_kde.sh
@@ -31,32 +31,73 @@ cd "$SCRIPT_DIR" || exit
 
 # ---------------------------------------------------------------------
 
-echo -e "$HEADER Configuring KDE $HEADER"
 
-if [[ $DESKTOP_SESSION != "plasma" && $DESKTOP_SESSION != "plasmawayland" ]]; then
-    echo -e "$NOTE KDE Plasma is not running. Configuring not required."
-    exit
+
+# echo -e "$HEADER Configuring KDE $HEADER"
+
+# if [[ $DESKTOP_SESSION != "plasma" && $DESKTOP_SESSION != "plasmawayland" ]]; then
+#     echo -e "$NOTE KDE Plasma is not running. Configuring not required."
+#     exit
+# fi
+
+# echo -e "$INFO Installing themes..."
+
+# cp -r kde/themes/* ~/.local/share
+
+# echo -e "$INFO Unpacking icons..."
+
+# cd ~/.local/share/icons/
+# for f in *.tar.gz; do tar xf "$f"; done
+# rm *.tar.gz
+# cd "$SCRIPT_DIR"
+
+# echo -e "$INFO Installing cursors..."
+
+# mkdir -p ~/.icons
+# cp -r kde/cursors/* ~/.icons
+
+
+# echo -e "$INFO Configuring options..."
+
+# cp -r kde/config/* ~/.config
+
+
+
+
+# echo -e "$INFO Configuring KDE theme settings..."
+
+config_file="$HOME/.config/kwinrc"
+
+theme=Breeze
+ButtonsOnLeft=M
+ButtonsOnRight=IAX
+
+
+if [ ! -f "$config_file" ]; then
+    echo -e "$INFO $config_file does not exist"
 fi
 
-echo -e "$INFO Installing themes..."
+if ! grep -q "\[org.kde.kdecoration2\]" "$config_file"; then
+    echo "[org.kde.kdecoration2]" >> "$config_file"
+fi
 
-cp -r kde/themes/* ~/.local/share
+if ! grep -q "^theme=" "$config_file"; then
+    sed -i '/\[org.kde.kdecoration2\]/a theme='"$theme"'' "$config_file"
+else
+    sed -i '/\[org.kde.kdecoration2\]/,/^\[/ s/^theme=.*/theme='"$theme"'/' "$config_file"
+fi
 
-echo -e "$INFO Unpacking icons..."
+if ! grep -q "^ButtonsOnLeft=" "$config_file"; then
+    sed -i '/\[org.kde.kdecoration2\]/a ButtonsOnLeft='"$ButtonsOnLeft"'' "$config_file"
+else
+    sed -i '/\[org.kde.kdecoration2\]/,/^\[/ s/^ButtonsOnLeft=.*/ButtonsOnLeft='"$ButtonsOnLeft"'/' "$config_file"
+fi
 
-cd ~/.local/share/icons/
-for f in *.tar.gz; do tar xf "$f"; done
-rm *.tar.gz
-cd "$SCRIPT_DIR"
+if ! grep -q "^ButtonsOnRight=" "$config_file"; then
+    sed -i '/\[org.kde.kdecoration2\]/a ButtonsOnRight='"$ButtonsOnRight"'' "$config_file"
+else
+    sed -i '/\[org.kde.kdecoration2\]/,/^\[/ s/^ButtonsOnRight=.*/ButtonsOnRight='"$ButtonsOnRight"'/' "$config_file"
+fi
 
-echo -e "$INFO Installing cursors..."
-
-mkdir -p ~/.icons
-cp -r kde/cursors/* ~/.icons
-
-
-echo -e "$INFO Configuring options..."
-
-cp -r kde/config/* ~/.config
 
 echo -e "$OK DONE"
