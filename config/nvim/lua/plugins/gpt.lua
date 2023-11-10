1bf4ab21145f2fb29d4ab863469fbb41029cfab9
Dmitry Savosh <d.savosh@gmail.com>
2023-11-10 03:07:46 +0300

    lazyvim

---
+++ config/nvim/lua/plugins/gpt.lua
@@ -0,0 +1,17 @@
+return {
+    -- codeium
+    {
+        'Exafunction/codeium.vim',
+        event = 'BufEnter',
+        config = function ()
+            -- Change '<C-g>' here to any keycode you like.
+            vim.keymap.set('i', '<c-right>', function () return vim.fn['codeium#Accept']() end, { expr = true })
+            vim.keymap.set('i', '<a-right>', function () return vim.fn['codeium#Accept']() end, { expr = true })
+            vim.keymap.set('i', '<c-]>', function() return vim.fn['codeium#CycleCompletions'](1) end, { expr = true })
+            vim.keymap.set('i', '<c-[>', function() return vim.fn['codeium#CycleCompletions'](-1) end, { expr = true })
+            vim.keymap.set('i', '<c-x>', function() return vim.fn['codeium#Clear']() end, { expr = true })
+          end
+    },
+
+  }
+  
