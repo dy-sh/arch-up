0f029323f5e6e4bc04c99214863f09947c0f937f
Dmitry Savosh <d.savosh@gmail.com>
2023-11-18 03:41:27 +0300

    vim

--- config/nvim/lua/config/keymaps.lua
+++ config/nvim/lua/config/keymaps.lua
@@ -1,3 +1,6 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+-- Use P instead of p to preserve yank register
+vim.keymap.set("v", "p", "P", { silent = true })
