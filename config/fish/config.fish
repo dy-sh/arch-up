6cb8815eea36e4bedd0888045a5359b03e8cab6a
Dmitry Savosh <d.savosh@gmail.com>
2023-11-28 21:02:26 +0300

    xplr

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -18,7 +18,18 @@ set -Ux GTK_USE_PORTAL 1
 alias cp '/usr/bin/advcp -g'
 alias mv '/usr/bin/advmv -g'
 
+
+# navigate bu tere util (cd on quit from tere)
+# run "tere" to naviafate
 function tere
     set --local result (command tere $argv)
     [ -n "$result" ] && cd -- "$result"
 end
+
+
+# navigate bu xplr (cd on quit from tere)
+# run "xcd" to naviafate
+function xcd
+    cd (xplr --print-pwd-as-result)
+end
+
