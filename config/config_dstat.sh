f4a40e980402081b19fa7e4f4a2ba8d6142b53b0
Dmitry Savosh <d.savosh@gmail.com>
2023-12-03 06:15:29 +0300

    dstat

---
+++ config/config_dstat.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+# Copyright Dmitry Savosh (d.savosh@gmail.com)
+
+# e - stop script if error
+# u - stop script if using uninitialized variable
+set -eu
+
+# set colors
+NC=$(tput sgr0)
+BOLD=$(tput bold)
+RED="${BOLD}$(tput setaf 1)"
+GREEN="${BOLD}$(tput setaf 2)"
+YELLOW="${BOLD}$(tput setaf 3)"
+BLUE="${BOLD}$(tput setaf 4)"
+MAGENTA="${BOLD}$(tput setaf 5)"
+CYAN="${BOLD}$(tput setaf 6)"
+WHITE="${BOLD}$(tput setaf 7)"
+# set message tags
+UP="\e[1A\e[K"
+INFO="[....]"
+NOTE="[${WHITE}NOTE${NC}]"
+OK="[${GREEN} OK ${NC}]"
+ACTN="[${CYAN}ACTN${NC}]"
+WARN="[${YELLOW}WARN${NC}]"
+ERR="[${RED}ERR!${NC}]"
+HEADER="${BLUE}\n--------------------------------------------------------------${NC}\n"
+
+# cd to script directory
+SCRIPT_DIR=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &>/dev/null && pwd)
+cd "$SCRIPT_DIR" || exit
+
+# ---------------------------------------------------------------------
+
+echo -e "$HEADER Configuring dstat $HEADER"
+
+# plugins
+yay -S --mflags --skipinteg --needed --quiet --answerdiff=None --nopgpfetch --nodiffmenu --norebuild --noredownload \
+	python-nvidia-ml-py
+
+# make temp dir
+dst=$(mktemp -d)
+cd $dst
+
+echo -e "$INFO Installing dstat plugins"
+
+wget https://raw.githubusercontent.com/datumbox/dstat/master/plugins/dstat_nvidia_gpu.py
+sudo mv dstat_nvidia_gpu.py /usr/share/dstat/
+
+wget https://raw.githubusercontent.com/xplorld/dstat_plugins/master/dstat_nvidia_gpu_mem.py
+sudo mv dstat_nvidia_gpu_mem.py /usr/share/dstat/
+
+echo -e "$OK DONE"
