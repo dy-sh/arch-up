00a90ad6ebfa88ecce9eb9b752335da0fedd0282
Dmitry Savosh <d.savosh@gmail.com>
2023-12-17 16:02:36 +0300

    nushell

--- config/config_nushell.sh
+++ config/config_nushell.sh
@@ -33,6 +33,7 @@ cd "$SCRIPT_DIR" || exit
 
 echo -e "$HEADER Configuring nushell $HEADER"
 
+echo -e "$INFO Installing dependencies"
 sudo pacman -S --needed --noconfirm starship zoxide broot
 
 # broot
@@ -40,6 +41,42 @@ broot --install
 # fix for nushell
 sed -i 's/def-env/def --env/' ~/.local/share/broot/launcher/nushell/4
 
+# config
+echo -e "$INFO Configuring nushell"
 cp -rT nushell ~
+
+# plugins
+echo -e "$INFO Installing plugins"
+
+# https://crates.io/crates/nu_plugin_net
+cargo install nu_plugin_net
+nu -c "register ~/.cargo/bin/nu_plugin_net"
+
+# https://github.com/fdncred/nu_plugin_regex
+# FAILED TO COMPILE !
+# cd $(mktemp -d)
+# git clone https://github.com/fdncred/nu_plugin_regex.git
+# cd nu_plugin_regex
+# cargo build --release
+# nu -c "register ./target/release/nu_plugin_regex"
+
+# https://github.com/Euphrasiologist/nu_plugin_plot
+cd $(mktemp -d)
+git clone https://github.com/Euphrasiologist/nu_plugin_plot
+cd nu_plugin_plot
+cargo build --release
+nu -c "register $(pwd)/target/release/nu_plugin_plot"
+# FAILED TO REGISTER:
+# cargo install nu_plugin_plot
+# nu -c "register ~/.cargo/bin/nu_plugin_plot"
+
+# https://github.com/Trivernis/nu-plugin-dialog
+# FAILED TO REGISTER !
+# cargo install nu_plugin_dialog
+# nu -c "register ~/.cargo/bin/nu_plugin_dialog"
+
+# https://github.com/cptpiepmatz/nu-plugin-highlight
+cargo install nu_plugin_highlight
+nu -c "register ~/.cargo/bin/nu_plugin_highlight"
 
 echo -e "$OK DONE"
