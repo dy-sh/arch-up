b7ec8fa8a00099a7859358fa6c9aebf7ce97d114
Dmitry Savosh <d.savosh@gmail.com>
2024-02-26 21:29:54 +0300

    -

--- config/config_btrfs.sh
+++ config/config_btrfs.sh
@@ -30,8 +30,8 @@ SCRIPT_DIR="$(dirname "$(realpath "$0")")"
 cd "$SCRIPT_DIR" || exit 1
 
 # reopen script as sudo
-SCRIPT_NAME="$(basename $0)"
 if [ $(id -u) -ne 0 ]; then
+    SCRIPT_NAME="$(basename $0)"
     echo "This script must be run as root: '$SCRIPT_DIR/$SCRIPT_NAME'"
     exec sudo "$SCRIPT_DIR/$SCRIPT_NAME" "$@"
 fi
