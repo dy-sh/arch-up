1f36cc1631e9a28e0e47db5954ad793dfd1f4539
Dmitry Savosh <d.savosh@gmail.com>
2024-02-27 02:52:47 +0300

    sddm

---
+++ config/config_sddm.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+# Copyright Dmitry Savosh (d.savosh@gmail.com)
+
+# e - stop script if error
+# u - stop script if using uninitialized variable
+set -eu 
+
+# set colors
+NC=$(tput sgr0)
+BOLD=$(tput bold)
+RED="${BOLD}$(tput setaf 1)"
+GREEN="${BOLD}$(tput setaf 2)"
+YELLOW="${BOLD}$(tput setaf 3)"
+BLUE="${BOLD}$(tput setaf 4)"
+MAGENTA="${BOLD}$(tput setaf 5)"
+CYAN="${BOLD}$(tput setaf 6)"
+WHITE="${BOLD}$(tput setaf 7)"
+# set message tags
+UP="\e[1A\e[K"
+INFO="[....]"
+NOTE="[${WHITE}NOTE${NC}]"
+OK="[${GREEN} OK ${NC}]"
+ACTN="[${CYAN}ACTN${NC}]"
+WARN="[${YELLOW}WARN${NC}]"
+ERR="[${RED}ERR!${NC}]"
+HEADER="${BLUE}\n--------------------------------------------------------------${NC}\n"
+
+# cd to script directory
+SCRIPT_DIR="$(dirname "$(realpath "$0")")"
+cd "$SCRIPT_DIR" || exit 1
+
+# ---------------------------------------------------------------------
+
+echo -e "$HEADER Configuring sddm $HEADER"
+
+sudo mkdir -p /etc/sddm.conf.d
+sudo cp sddm/kde_settings.conf /etc/sddm.conf.d/
+
+
+echo -e "$OK DONE"
